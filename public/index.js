4f7d06c612feedb77108ab2bcd8270d0b3592023
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-10-27 10:30:57 -0400

    fixed license badge

--- public/index.js
+++ public/index.js
@@ -126,7 +126,7 @@ function sendTransaction(isAdding) {
   })
   .then(data => {
     if (data.errors) {
-      errorEl.textContent = "Missing Information";
+      errorEl.textContent = "Missing Information !";
     }
     else {
       // clear form
